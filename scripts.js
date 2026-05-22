413707b040230987af2488ed7481acd1b4dc00e5
hub-mgv <fire1230vvvvg@gmail.com>
2026-05-22 06:40:30 -0900

    refactor: migrate project architecture from core/ to src/ and update all internal imports and aliases

--- scripts.js
+++ scripts.js
@@ -256,7 +256,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     async function data() {
         try {
-            const response = await fetch('./core/json/reciters.json');
+            const response = await fetch('./core/src/reciters.json');
             if (!response.ok) throw new Error('Failed to load database');
             const data = await response.json();
             const cachedData = data.cached_data || data;
